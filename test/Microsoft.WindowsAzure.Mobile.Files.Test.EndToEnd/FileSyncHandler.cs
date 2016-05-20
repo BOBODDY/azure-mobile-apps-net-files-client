7305e67a7fbf38e39555208c7c93c1616f179c3f
danderson00 <danderson00@gmail.com>
2016-05-19 22:00:44 -0700

    Added e2e test project

---
+++ test/Microsoft.WindowsAzure.Mobile.Files.Test.EndToEnd/FileSyncHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.MobileServices.Files.Sync;
+using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices.Files;
+using Microsoft.WindowsAzure.MobileServices.Files.Metadata;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Microsoft.WindowsAzure.Mobile.Files.Test.EndToEnd
+{
+    class FileSyncHandler : IFileSyncHandler
+    {
+        private readonly Dictionary<string, string> Sources;
+
+        public FileSyncHandler(Dictionary<string, string> sources)
+        {
+            Sources = sources;
+        }
+
+        public async Task<IMobileServiceFileDataSource> GetDataSource(MobileServiceFileMetadata metadata)
+        {
+            return new StreamDataSource(Sources[metadata.FileName]);
+        }
+
+        public async Task ProcessFileSynchronizationAction(MobileServiceFile file, FileSynchronizationAction action)
+        {
+
+        }
+    }
+}
---
+++ test/Microsoft.WindowsAzure.Mobile.Files.Test.EndToEnd/StreamDataSource.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.WindowsAzure.MobileServices.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Microsoft.WindowsAzure.Mobile.Files.Test.EndToEnd
+{
+    class StreamDataSource : IMobileServiceFileDataSource
+    {
+        private readonly string Source;
+
+        public StreamDataSource(string source)
+        {
+            Source = source;
+        }
+
+        public async Task<Stream> GetStream()
+        {
+            return new MemoryStream(Source.Select(x => (byte)x).ToArray());
+        }
+    }
+}
