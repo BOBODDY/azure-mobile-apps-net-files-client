b6814b8c3cba6b5e5078ab6147b239900b175b45
danderson00 <danderson00@gmail.com>
2016-05-27 18:17:12 -0700

    Added default locations for platforms and initialize overloads

--- src/Microsoft.WindowsAzure.Mobile.Files.Shared/Managed/FileSystemAccess.cs
+++ src/Microsoft.WindowsAzure.Mobile.Files.Shared/Managed/FileSystemAccess.cs
@@ -1,49 +1,37 @@
-﻿using System;
+﻿// ---------------------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
 using System.IO;
 using System.Threading.Tasks;
-#if WIN_APPS
-using Windows.Storage;
-#endif
 
 namespace Microsoft.WindowsAzure.MobileServices.Files.Managed.LocalStorage.FileSystem
 {
+#if !WIN_APPS
     public class FileSystemAccess : IFileSystemAccess
     {
-#if __IOS__ || __UNIFIED__ || __ANDROID__ || DOTNET
+        private readonly string basePath;
+
+        public FileSystemAccess(string basePath = "")
+        {
+            this.basePath = basePath;
+        }
+
         public Task<Stream> CreateAsync(string targetPath)
         {
-            return Task.FromResult<Stream>(File.Create(targetPath));
+            return Task.FromResult<Stream>(File.Create(Path.Combine(basePath, targetPath)));
         }
 
         public Task<Stream> OpenReadAsync(string targetPath)
         {
-            return Task.FromResult<Stream>(File.OpenRead(targetPath));
+            return Task.FromResult<Stream>(File.OpenRead(Path.Combine(basePath, targetPath)));
         }
 
         public Task DeleteAsync(string targetPath)
         {
-            File.Delete(targetPath);
+            File.Delete(Path.Combine(basePath, targetPath));
             return Task.FromResult(true);
         }
-#elif WIN_APPS
-        public async Task<Stream> CreateAsync(string targetPath)
-        {
-            StorageFile file = await StorageFile.GetFileFromPathAsync(targetPath);
-
-            return await file.OpenStreamForWriteAsync();
-        }
-
-        public async Task<Stream> OpenReadAsync(string targetPath)
-        {
-            StorageFile file = await StorageFile.GetFileFromPathAsync(targetPath);
-
-            return await file.OpenStreamForWriteAsync();
-        }
-
-        public Task DeleteAsync(string targetPath)
-        {
-            throw new NotImplementedException();
-        }
-#endif
     }
+#endif
 }
---
+++ src/Microsoft.WindowsAzure.Mobile.Files.WinApps/Managed/WinAppsFileSystemAccess.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Microsoft.WindowsAzure.MobileServices.Files.Managed.LocalStorage.FileSystem
+{
+    public class WinAppsFileSystemAccess : IFileSystemAccess
+    {
+        private readonly string basePath;
+
+        public WinAppsFileSystemAccess(string basePath = "")
+        {
+            this.basePath = basePath;
+        }
+
+        public async Task<Stream> CreateAsync(string targetPath)
+        {
+            StorageFile file = await StorageFile.GetFileFromPathAsync(Path.Combine(basePath, targetPath));
+            return await file.OpenStreamForWriteAsync();
+        }
+
+        public async Task<Stream> OpenReadAsync(string targetPath)
+        {
+            StorageFile file = await StorageFile.GetFileFromPathAsync(Path.Combine(basePath, targetPath));
+            return await file.OpenStreamForReadAsync();
+        }
+
+        public async Task DeleteAsync(string targetPath)
+        {
+            StorageFile file = await StorageFile.GetFileFromPathAsync(Path.Combine(basePath, targetPath));
+            await file.DeleteAsync();
+        }
+    }
+}
